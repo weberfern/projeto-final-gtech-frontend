4ed783c028a0bdf40be1b5b5bb3b031cc857df8e
weberfern <weber12@gmail.com>
2026-03-18 23:02:40 -0300

    feat: finaliza a ProductViewPage com a BuyBox, opções de produto e CSS refatorado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import HomePage from './pages/HomePage';
 import ProductListingPage from './pages/ProductListingPage';
+import ProductViewPage from './pages/ProductViewPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         } />
 
         <Route path="/products" element={<ProductListingPage />} />
+        <Route path="/product" element={<ProductViewPage />} />
       </Routes>
     </BrowserRouter>
   );
